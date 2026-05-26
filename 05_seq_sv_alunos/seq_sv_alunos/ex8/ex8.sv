6ed6961c5a40110a007fbcafbbf29e4f05e1a0d1
Luísa Winimko Beck <l.beck02@edu.pucrs.br>
2026-05-26 09:28:35 -0300

    Add files via upload

--- 05_seq_sv_alunos/seq_sv_alunos/ex8/ex8.sv
+++ 05_seq_sv_alunos/seq_sv_alunos/ex8/ex8.sv
@@ -7,6 +7,30 @@ module ex8 (
     input  logic [3:0]  entrada,
     output logic [3:0]  saida
 );
+    logic [3:0] A, B, C, D;
+    logic [1:0] contIn, contOut;
+    logic [17:0] vet, regSoma;
+
+    always_ff @(posedge clock or posedge reset) begin
+        if (reset) begin
+            contIn <= 2'b00;
+            A <= '0;
+            B <= '0;
+            C <= '0;
+            D <= '0;
+        end else if (send) begin
+            contIn <= contIn + 1;
+            A <= entrada;
+            B <= A;
+            C <= B;
+            D <= C;
+        end
+    end
+
+    assign vet = {A, B, C, D};
+
+
+
 
    /// completar
 
